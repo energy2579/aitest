d9f4037a4b6074d4a37d698895d2f72e1eceb0de
energy2579 <energy2579@hotmail.com>
2023-02-23 17:47:48 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -8,8 +8,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        #self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
-        self.model = os.getenv("OPENAI_MODEL", default = "chatbot")
+        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        #self.model = os.getenv("OPENAI_MODEL", default = "chatbot")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
@@ -24,7 +24,10 @@ class ChatGPT:
             presence_penalty=self.presence_penalty,
             max_tokens=self.max_tokens
         )
-        return 1 #response['choices'][0]['text'].strip()
+        return 0 #response['choices'][0]['text'].strip()
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
+
+    def aa():
+        return 1
